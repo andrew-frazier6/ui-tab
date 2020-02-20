0661f2aca182599e0cfab66034986f44778c534f
Andrew Frazier <acorbettfrazier@gmail.com>
2020-02-20 15:17:59 -0500

    api attached

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Header from "./Header";
 import Footer from "./Footer";
-import Tab from "./Tab";
+import Tab from "./Tab.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <div className="tab_container">
-        <Tab />
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      data: ""
+    };
+  }
+
+  componentDidMount() {
+    this.getData();
+  }
+
+  getData = () => {
+    let url =
+      "https://developer.nps.gov/api/v1/places?api_key=bRrLhFfbvZMWxHlCWDqPt45vwoNgz950IaYNYkmC";
+    fetch(url)
+      .then(res => res.json())
+      .then(data => {
+        this.setState({ data: data });
+        console.log(data);
+      });
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="tab_container">
+          <Tab />
+        </div>
+        <Footer />
       </div>
-      <Footer />
-    </div>
-  );
+    );
+  }
 }
 
 export default App;
--- src/Tab.js
+++ src/Tab.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { Component } from "react";
+import "./Tab.css";
 
 function Tab() {
-  return <div></div>;
+  return (
+    <div className="tab_box">
+      <nav>
+        <ul>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </nav>
+      <div className="info_box"></div>
+    </div>
+  );
 }
 
 export default Tab;
